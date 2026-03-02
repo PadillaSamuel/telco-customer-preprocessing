6a48191d99b3b426901b89abc413fbf9b165cefa
SamPaCe05 <samuelpadilla2605@gmail.com>
2026-03-01 23:07:31 -0500

    data preprocessing and descisionTreeClassifier

---
+++ pipeline/preprocessing.py
@@ -0,0 +1,96 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder
+from sklearn.pipeline import Pipeline 
+from sklearn.compose import ColumnTransformer  
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.impute import SimpleImputer
+
+caracteristicasNumericas = ['tenure', 'MonthlyCharges', 'TotalCharges']
+caracteristicasNominales = ['PaymentMethod', 'InternetService']
+caracteristicasBinarias= ['gender' ,'SeniorCitizen' , 'Partner' ,'StreamingMovies', 
+                                'StreamingTV', 'TechSupport', 'DeviceProtection', 'OnlineBackup', 
+                                'OnlineSecurity' ,'Dependents', 'PhoneService', 'MultipleLines', 'PaperlessBilling']
+caracteristicasOrdinales =['Contract']
+
+def leerDataset():
+    return pd.read_csv('../data/WA_Fn-UseC_-Telco-Customer-Churn.csv')
+    
+def arbolDesicion():
+    preprocesador  = construirPipeline()
+    arbolDesicion = DecisionTreeClassifier(random_state=1,criterion='entropy', class_weight='balanced' ,max_depth=3)
+    pipeline = Pipeline(
+        steps=[
+            ('preprocesador', preprocesador),
+            ('arbolDesicion', arbolDesicion)
+        ]
+    )
+    return pipeline
+
+
+def construirPipeline():
+    transformadorNumerico = Pipeline(
+    steps=[
+        # ('imputer', SimpleImputer(strategy= 'median')),
+        ('scalar', StandardScaler())        
+    ]
+    )
+    contractOrden = [['Month-to-month', 'One year', 'Two year']]
+    
+    transformadorOrdinal = Pipeline(
+        steps=[
+            ('ordinal', OrdinalEncoder(categories=contractOrden))         
+        ]
+    )
+
+    transformadorNominal = Pipeline(
+        steps=[
+            ('oneHot', OneHotEncoder(handle_unknown='ignore'))
+        ]
+    )
+    transformadorBinario= Pipeline(
+        steps=[
+            ('binario', OrdinalEncoder())
+        ]
+    )
+    preprocesador = ColumnTransformer(
+    transformers=[
+        ('binario', transformadorBinario, caracteristicasBinarias),
+        ('numerico', transformadorNumerico, caracteristicasNumericas),
+        ('ordinal', transformadorOrdinal, caracteristicasOrdinales), 
+        ('nominal', transformadorNominal, caracteristicasNominales)
+    ]
+        )
+    return preprocesador
+
+def separarCaracteristicas(df, objetivo):   
+    
+    df= limpiarDatos(df)    
+    # dfTargetYes= df[df[objetivo]=='Yes']
+    # dfTargetNo= df[df[objetivo]=='No']
+    
+    # dfChurnNo = dfTargetNo.sample(len(dfTargetYes), random_state=1)
+    # dfBalanceado = pd.concat([dfChurnNo, dfTargetYes]) 
+    
+    # target = dfBalanceado[objetivo]
+    
+    # caracteristicas = dfBalanceado.drop(columns=[objetivo, 'customerID'])
+    caracteristicas= df.drop(columns=[objetivo, 'customerID'])
+    target = df[objetivo]
+    
+    return caracteristicas, target
+
+def limpiarDatos(df):
+    
+    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors= 'coerce')
+    df['MonthlyCharges'] = pd.to_numeric(df['MonthlyCharges'], errors= 'coerce')
+    
+    df.dropna(inplace=True)
+    
+    df['MultipleLines'] = df['MultipleLines'].replace('No phone service', 'No')    
+    
+    columnasInternet = ['StreamingMovies', 'StreamingTV', 'TechSupport', 'DeviceProtection', 'OnlineBackup', 'OnlineSecurity',]
+
+    for col in columnasInternet:
+        df[col]= df[col].replace('No internet service', 'No')
+    
+    return df
